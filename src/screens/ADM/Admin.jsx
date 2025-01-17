8c7d814b267a1ecca8c315ea5699283e4ae4a413
João Marcos <joaomarcosjoao92@gmail.com>
2025-01-16 22:53:55 -0300

    correções na tela de adm

--- src/screens/ADM/Admin.jsx
+++ src/screens/ADM/Admin.jsx
@@ -6,48 +6,52 @@ import userIconPng from "./imagens/userIcon.png";
 import movieIconPng from "./imagens/movieIcon.png";
 
 const Admin = () => {
-  document.title = "Administrativo CineHouse"; // hook para alterar titulo da aba
+  document.title = "Administrativo CineHouse"; // hook para alterar título da aba
+
   return (
-    <div className="container">
-      <div className="card">
-        <div className="card-header">Gerenciar usuários</div>
-        <div className="card-body">
-          <img
-            src={userIconPng}
-            alt="Ícone de Gerenciar Usuários"
-            className="card-image"
-          />
-          <button>Adicionar usuário</button>
-          <button>Editar usuário</button>
-          <button>Buscar usuário</button>
+    <div>
+      {/* Container dos cards */}
+      <div className="container">
+        <div className="card">
+          <div className="card-header">Gerenciar usuários</div>
+          <div className="card-body">
+            <img
+              src={userIconPng}
+              alt="Ícone de Gerenciar Usuários"
+              className="card-image"
+            />
+            <button>Adicionar usuário</button>
+            <button>Editar usuário</button>
+            <button>Buscar usuário</button>
+          </div>
         </div>
-      </div>
 
-      <div className="card">
-        <div className="card-header">Gerar relatório</div>
-        <div className="card-body">
-          <img
-            src={pranchetaPng}
-            alt="Ícone de Gerenciar Usuários"
-            className="card-image"
-          />
-          <button>Relatório de finanças</button>
-          <button>Relatório de vendas/alugéis</button>
-          <button>Relatório de devoluções</button>
+        <div className="card">
+          <div className="card-header">Gerar relatório</div>
+          <div className="card-body">
+            <img
+              src={pranchetaPng}
+              alt="Ícone de Gerar Relatório"
+              className="card-image"
+            />
+            <button>Relatório de finanças</button>
+            <button>Relatório de vendas/alugéis</button>
+            <button>Relatório de devoluções</button>
+          </div>
         </div>
-      </div>
 
-      <div className="card">
-        <div className="card-header">Editar Filmes</div>
-        <div className="card-body">
-          <img
-            src={movieIconPng}
-            alt="Ícone de Gerenciar Usuários"
-            className="card-image"
-          />
-          <button>Adicionar filmes</button>
-          <button>Editar filme</button>
-          <button>Remover filme</button>
+        <div className="card">
+          <div className="card-header">Editar Filmes</div>
+          <div className="card-body">
+            <img
+              src={movieIconPng}
+              alt="Ícone de Editar Filmes"
+              className="card-image"
+            />
+            <button>Adicionar filmes</button>
+            <button>Editar filme</button>
+            <button>Remover filme</button>
+          </div>
         </div>
       </div>
     </div>
