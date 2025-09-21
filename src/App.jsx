1739efdbb730a90759cfc3592e251199959a809d
Layon Walker <layon.pedro@estudante.ufla.br>
2025-09-21 10:17:22 -0300

    Trocando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-/* eslint-disable no-unused-vars */
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ADMIN_ROUTES } from './config/adminRoutes'; // 1. Importa as rotas
+import { USER_ROUTES } from './config/userRoutes'; 
+import { PUBLIC_ROUTES} from './config/publicRoutes';
+
+// Importação das páginas.
 import LoginPage from "./screens/Login/index.jsx";
 import Recover from "./screens/Recover/index.jsx";
-import Register from "./screens/Register/index.jsx";
+import RegisterPage from "./screens/Register/index.jsx";
 import Home from "./screens/Home/index.jsx";
 import UserSettings from "./screens/UserSettings/UserSettings";
 import Admin from "./screens/AdminDashboardPage/index.jsx";
@@ -14,28 +18,41 @@ import RelatorioDevolucao from "./screens/Relatorios/Devolucoes/Relatorio";
 import CriarUsuario from "./screens/adm/CriarUsuario/index.jsx";
 import GerenciarFilme from "./screens/adm/GerenciarFilme/index.jsx";
 import RecuperarSenha from "./screens/RecuperarSenha/RecuperarSenha.jsx"
-import './App.css'; // Importando o CSS global
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/recover" element={<Recover />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/userSettings" element={<UserSettings />} />
-        <Route path="/adm" element={<Admin />} />
-        <Route path="/relatorio/alugueisEVendas" element={<Relatorio />} />
-        <Route path="/relatorio/devolucao" element={<RelatorioDevolucao />} />
-        <Route path="/histPedidos" element={<HistPedidos />} />
-        <Route path="/gerenciarUsuarios" element={<GerenciarUsuarios />} />
-        <Route path="/gerenciarFilme" element={<GerenciarFilme />} />
-        <Route path="/adm/criarUsuario" element={<CriarUsuario />} />
-        <Route path="/recuperarSenha" element={<RecuperarSenha />} />
-        <Route path="/" element={<LoginPage />} />
+        {/* Rotas Públicas */}
+        <Route path={PUBLIC_ROUTES.LOGIN} element={<LoginPage />} />
+        <Route path={PUBLIC_ROUTES.REGISTER} element={<RegisterPage />} />
+        <Route path={PUBLIC_ROUTES.RECOVER} element={<Recover />} />
+        {/* ... */}
+        
+        {/* Rotas Administrativas - AGORA USANDO AS CONSTANTES */}
+        <Route path={ADMIN_ROUTES.DASHBOARD} element={<Admin />} />
+        <Route path={ADMIN_ROUTES.CREATE_USER} element={<CriarUsuario />} />
+        <Route path={ADMIN_ROUTES.MANAGE_USERS} element={<GerenciarUsuarios />} />
+        <Route path={ADMIN_ROUTES.MANAGE_MOVIES} element={<GerenciarFilme />} />
+        <Route path={ADMIN_ROUTES.REPORTS_SALES} element={<Relatorio />} />
+        <Route path={ADMIN_ROUTES.REPORTS_RETURNS} element={<RelatorioDevolucao />} />
+        {/* Rotas do Usuário */}
+        <Route path={USER_ROUTES.HOME} element={<Home />} />
+        <Route path={USER_ROUTES.SETTINGS} element={<UserSettings />} />
+        <Route path={USER_ROUTES.ORDER_HISTORY} element={<HistPedidos />} />
+        <Route path={USER_ROUTES.RECOVER_PASSWORD} element={<RecuperarSenha />} />
+
+        
+        {/* ... resto das suas rotas */}
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
+
+
+
+
---
+++ src/config/userRoutes.js
@@ -0,0 +1,10 @@
+
+const USER_ROUTES = {
+    HOME: '/home',
+    PROFILE: '/profile',
+    SETTINGS: '/settings',
+    ORDER_HISTORY: '/history',
+    BROWSE_MOVIES: '/movies',
+}
+
+export { USER_ROUTES };
